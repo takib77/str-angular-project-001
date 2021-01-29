4ca0a40684c6ad0e99881610ea9dd7dd356fd299
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2021-01-29 22:14:08 +0100

    Img URL

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -9,91 +9,91 @@ export class ProductService {
   list: Animal[] = [{
     id: 0,
     name: "Barna Medve",
-    image: "",
+    image: "src/assets/img/barnamedve.jpg",
     price: 100000,
     endangered: true
   }, {
     id: 1,
     name: "Leveli Beka",
-    image: "",
+    image: "src/assets/img/beka.jpg",
     price: 10000,
     endangered: false
   }, {
     id: 2,
     name: "Bivaly",
-    image: "",
+    image: "src/assets/img/bivaly.jpg",
     price: 150000,
     endangered: false
   }, {
     id: 3,
     name: "Elefánt",
-    image: "",
+    image: "src/assets/img/elefant.jpg",
     price: 10000,
     endangered: true
   }, {
     id: 4,
     name: "Gorilla",
-    image: "",
+    image: "src/assets/img/gorilla.jpg",
     price: 100000,
     endangered: false
   }, {
     id: 5,
     name: "Kazuár",
-    image: "",
+    image: "src/assets/img/kazuár.jpg",
     price: 100000,
     endangered: false
   }, {
     id: 6,
     name: "Tapír",
-    image: "",
+    image: "src/assets/img/tapir.jpg",
     price: 10000,
     endangered: false
   }, {
     id: 7,
     name: "Tigris",
-    image: "",
+    image: "src/assets/img/tigris.jpg",
     price: 2000000,
     endangered: true
   }, {
     id: 8,
-    name: "Oroszlán",
-    image: "",
+    name: "Piranja",
+    image: "src/assets/img/piranja.jpg",
     price: 560000,
     endangered: false
   }, {
     id: 9,
     name: "Orrszarvú",
-    image: "",
+    image: "src/assets/img/orrszarvu.jpg",
     price: 890000,
     endangered: false
   }, {
     id: 10,
     name: "Szurikáta",
-    image: "",
+    image: "src/assets/img/szurikata.jpg",
     price: 10000,
     endangered: false
   }, {
     id: 11,
     name: "Rozsomák",
-    image: "",
+    image: "src/assets/img/rozsomak.jpg",
     price: 12000,
     endangered: true
   }, {
     id: 0,
     name: "Vadkutya",
-    image: "",
+    image: "src/assets/img/vadkutya.jpg",
     price: 19000,
     endangered: false
   }, {
     id: 12,
     name: "Víziló",
-    image: "",
+    image: "src/assets/img/vizilo.jpg",
     price: 10000,
     endangered: true
   }, {
     id: 13,
     name: "Wombat",
-    image: "",
+    image: "src/assets/img/wombat.jpg",
     price: 189000,
     endangered: false
   },
