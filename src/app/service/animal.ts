2d3ec186c8b6a94384c6d079c87ec1608cc0095c
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2021-01-29 21:27:09 +0100

    ProductService alakul.

--- src/app/service/animal.ts
+++ src/app/service/animal.ts
@@ -1,13 +1,19 @@
 
-export interface Animal {
-    id : number;
-    catID: number;
-    name: string;
-    description: string;
-    image: string;
-    price: number;
-    stock: number;
-    featured: boolean;
-    endangered: boolean;
-    
+export class Animal {
+    id : number = 0;
+    name: string ='';
+    image: string ='';
+    price: number =0;
+    endangered: boolean =true;   
+
+
+constructor(properties? : Animal) {
+        if (properties) {
+            this.id = properties.id || 0;
+            this.name = properties.name;
+            this.image = properties.image;
+            this.price = properties.price;
+            this.endangered = properties.endangered;
+        }
+    }
 }
