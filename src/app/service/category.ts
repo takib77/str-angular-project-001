366ff3da995ddd85be9d66cb4fac8c1aaff59775
Leo <takib77@gmail.com>
2021-01-29 19:25:14 +0100

    boolean list

--- src/app/service/category.ts
+++ src/app/service/category.ts
@@ -3,3 +3,63 @@ export class Category {
     name: string;
     description: string;
 }
+
+class List {
+    animals: any[] = [
+        { "featured": true, "active": false },
+        { "featured": true, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": true },
+        { "featured": false, "active": false },
+        { "featured": true, "active": false },
+        { "featured": false, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+        { "featured": true, "active": false },
+
+        { "featured": true, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+        { "featured": false, "active": false },
+        { "featured": true, "active": false },
+        { "featured": true, "active": true },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+
+        { "featured": true, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+        { "featured": false, "active": false },
+        { "featured": true, "active": false },
+        { "featured": true, "active": true },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+
+        { "featured": true, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+        { "featured": false, "active": false },
+        { "featured": true, "active": false },
+        { "featured": true, "active": true },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+
+        { "featured": true, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false },
+        { "featured": false, "active": false },
+        { "featured": true, "active": false },
+        { "featured": true, "active": true },
+        { "featured": false, "active": true },
+        { "featured": true, "active": false }
+
+    ]
+}
