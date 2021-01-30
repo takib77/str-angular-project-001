bda114e6ab8d27d174c0d338a29b690697b4b371
Leo <takib77@gmail.com>
2021-01-30 20:39:39 +0100

    components

---
+++ src/app/model/animal.ts
@@ -0,0 +1,17 @@
+export class Animal {
+    id: number = 0;
+    name: string = '';
+    image: string = '';
+    price: number = 0;
+    endangered: boolean = false;
+
+    constructor(properties?: Animal) {
+        if (properties) {
+            this.id = properties.id || 0;
+            this.name = properties.name || '';
+            this.image = properties.image || '';
+            this.price = properties.price || 0;
+            this.endangered = properties.endangered || false;
+        }
+    }
+}
