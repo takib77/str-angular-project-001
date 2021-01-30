c93300fce571b08c908f2ab00e4f21e169d8cd8d
Leo <takib77@gmail.com>
2021-01-30 21:19:47 +0100

    it's better

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -7,12 +7,6 @@ import { Animal } from '../model/animal';
 })
 export class ProductService {
   list: Animal[] = [{
-    id: 0,
-    name: "Barna Medve",
-    image: "src/assets/img/barnamedve.jpg",
-    price: 100000,
-    endangered: true
-  }, {
     id: 1,
     name: "Leveli Beka",
     image: "src/assets/img/beka.jpg",
@@ -96,8 +90,14 @@ export class ProductService {
     image: "src/assets/img/wombat.jpg",
     price: 189000,
     endangered: false
-  },
-  ]
+  }, {
+    id: 14,
+    name: "Barna Medve",
+    image: "src/assets/img/barnamedve.jpg",
+    price: 100000,
+    endangered: true
+  }
+  ];
 
   constructor() { }
 
